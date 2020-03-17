AOSPA/android_vendor_pa

tasks: bacon: Don't create identical otapackages
Saves space

Change-Id: I9b0472c617d695330614baf8b41afb918cac0dac
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

build/tasks/bacon.mk
@@ -22,6 +22,6 @@ PA_TARGET_PACKAGE := $(PRODUCT_OUT)/pa$(PA_VERSION).zip
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(PA_TARGET_PACKAGE)
+	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(PA_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(PA_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(PA_TARGET_PACKAGE).md5sum
 	@echo "Package Complete: $(PA_TARGET_PACKAGE)" >&2

aueUI/android_vendor_aueUI

Signed-off-by: gesangtome <gesangtome@foxmail.com>

build/tasks/bacon.mk
@@ -23,6 +23,6 @@ AUEUI_TARGET_PACKAGE := $(PRODUCT_OUT)/aueUI-$(AUEUI_VERSION).zip
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(AUEUI_TARGET_PACKAGE)
+	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(AUEUI_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(AUEUI_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(AUEUI_TARGET_PACKAGE).md5sum
 	@echo "Package Complete: $(AUEUI_TARGET_PACKAGE)" >&2