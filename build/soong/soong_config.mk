AOSPA/android_vendor_pa

aospa: add soong bits for boardflags
Change-Id: Iff6c08f1e356ddd9fa0f5ea3308c57df5f5db885

build/soong/soong_config.mk
@@ -1,0 +1,9 @@
+_contents := $(_contents)    "Aospa":{$(newline)
+
+# See build/core/soong_config.mk for the add_json_* functions you can use here.
+
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
+
+_contents := $(_contents)    },$(newline)

aueUI/android_vendor_aueUI

build/soong/soong_config.mk
@@ -1,0 +1,9 @@
+_contents := $(_contents)    "aueUI":{$(newline)
+
+# See build/core/soong_config.mk for the add_json_* functions you can use here.
+
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
+
+_contents := $(_contents)    },$(newline)